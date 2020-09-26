e81c937a17440fe376e0d7d84cd2892d28e938d8
Marko Elez <markoelez7@gmail.com>
2020-09-26 19:32:06 -0400

    resync gitignore

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nasm -f bin 32bit-main.asm -o boot_sect.bin
---
+++ compiler/lib/gcc/i686-elf/10.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Users/marko/basicos/src/gcc-10.1.0/configure --target=i686-elf --prefix=/Users/marko/basicos/compiler --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
