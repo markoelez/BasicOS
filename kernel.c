9bc0dccf379c19293ee8580b9ae7909b6f5a8da8
Marko Elez <markoelez7@gmail.com>
2020-09-28 13:46:38 -0400

    redundant comments

--- kernel.c
+++ kernel.c
@@ -2,17 +2,14 @@
 #include <stddef.h>
 #include <stdint.h>
  
-/* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#error "Must use a cross-compiler"
 #endif
  
-/* This tutorial will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#error "Must compile with a ix86-elf compiler"
 #endif
  
-/* Hardware text mode color constants. */
 enum vga_color {
 	VGA_COLOR_BLACK = 0,
 	VGA_COLOR_BLUE = 1,
@@ -96,9 +93,6 @@ void terminal_writestring(const char* data) {
 }
  
 void kernel_main(void) {
-	/* Initialize terminal interface */
 	terminal_initialize();
- 
-	/* Newline support is left as an exercise. */
 	terminal_writestring("Hello, kernel World!\n");
 }
